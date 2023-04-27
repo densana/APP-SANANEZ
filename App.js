be6445c12243b03e90fb1b85e39cd38f98f9cc9f
densana <denisesananez@gmail.com>
2023-04-27 09:43:57 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, TextInput, Button, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput placeholder='Ingresa tu compra aqui'></TextInput>
+        <Button title='Add'></Button>
+      </View>
     </View>
   );
 }
@@ -14,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
